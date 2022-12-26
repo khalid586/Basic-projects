a22b551bc0b39cf1fccca393c74ed00dd6c20e51
khalid <khalidcomilla58@gmail.com>
2022-12-26 22:55:37 +0600

    start button added in js file

--- stopwatch using html css js/script.js
+++ stopwatch using html css js/script.js
@@ -8,13 +8,30 @@ let seconds = 0;
 let interval = null;
 
 // Event listeners
+start_btn.addEventListener('click',start);
 
 // update the timer
 function timer(){
     seconds++;
 
     // Format out timer
-    let secs = seconds % 60;
+     hrs = Math.floor(seconds / 3600);
+     mins = Math.floor((seconds - (hrs * 3600))/60);
+     secs = seconds % 60;
     
+    if(secs < 10) secs = '0' + secs;
+    if(mins < 10) mins = '0' + mins;
+    if(hrs  < 10) hrs = '0' + hrs;
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+
 }
+timer();
+
+function start(){
+    if(interval){
+        return
+    }
+    interval = setInterval(timer,1000);
+}   
+
 
